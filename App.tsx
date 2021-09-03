11d72fb7a5b01bf4b9a5953b75930134f26caf12
Tonny <tonny035@hotmail.com>
2021-09-03 14:27:28 +0200

    React Native tutorial part 1

--- App.tsx
+++ App.tsx
@@ -1,25 +1,46 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
+ * React Native Tutorial - Todo App (part 1)
+ * - https://www.youtube.com/watch?v=uLHFPt9B2Os
  *
  * @format
  */
 
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header';
 
-const App = () => {
-  return <View style={styles.container}></View>;
-};
+export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on the pc', key: '3'},
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
-
-export default App;
