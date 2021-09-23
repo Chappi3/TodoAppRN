7de51beb8ab7c4d4c8e8972d21c5ea66cc1964e4
Tonny <tonny035@hotmail.com>
2021-09-23 08:18:47 +0200

    refactor

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import AppContainer from './AppContainer';
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
+import AppContainer from './AppContainer'
 
-AppRegistry.registerComponent(appName, () => AppContainer);
+AppRegistry.registerComponent(appName, () => AppContainer)
