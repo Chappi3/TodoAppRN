1a57e2533cdb78ebfcc0c6e6ebf9d5adec2df192
Tonny <tonny035@hotmail.com>
2021-09-23 08:18:11 +0200

    install redux & uuid

--- AppContainer.tsx
+++ AppContainer.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import App from './src/App';
-import {ThemeProvider} from './src/contexts/theme-context';
+import React from 'react'
+import App from './src/App'
+import { Provider } from 'react-redux'
+import { store } from './src/redux'
+import { ThemeProvider } from './src/contexts/theme-context'
 
 export default function AppContainer() {
   return (
     <ThemeProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
-  );
+  )
 }
